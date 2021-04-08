6a6a6fb50843d6f6486a7fde547381b875f5e8f2
MrVictor42 <guin-hu@hotmail.com>
2021-04-08 16:48:18 -0300

    Create and learning how to change screen

---
+++ BankMR/app/src/main/java/com/victor/bankmr/Balance.kt
@@ -0,0 +1,11 @@
+package com.victor.bankmr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Balance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balance)
+    }
+}

5b9f8c77040aa49a96f688613a0aa08fb2bee84f
MrVictor42 <guin-hu@hotmail.com>
2021-04-10 17:31:25 -0300

    Created toolbar netflix

--- NetflixClone/app/src/main/java/com/victor/netflixclone/FormRegister.kt
+++ NetflixClone/app/src/main/java/com/victor/netflixclone/FormRegister.kt
@@ -2,10 +2,19 @@ package com.victor.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.victor.netflixclone.databinding.ActivityFormRegisterBinding
 
 class FormRegister : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_register)
+        binding = ActivityFormRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+
     }
 }

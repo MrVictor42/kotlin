8656edebf1deadcfcb2202b8a5e1c4d03f2459ab
MrVictor42 <guin-hu@hotmail.com>
2021-04-10 17:00:43 -0300

    Finish screen login

--- NetflixClone/app/src/main/java/com/victor/netflixclone/FormLogin.kt
+++ NetflixClone/app/src/main/java/com/victor/netflixclone/FormLogin.kt
@@ -1,13 +1,24 @@
 package com.victor.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.victor.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        binding.registerText.setOnClickListener {
+            val intent = Intent(this, FormRegister::class.java)
+            startActivity(intent)
+        }
     }
 }

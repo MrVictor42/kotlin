5fa948a10199473c9a450d5129e52983d1a108cc
MrVictor42 <guin-hu@hotmail.com>
2021-04-11 18:41:35 -0300

    Logout netflix app

--- NetflixClone/app/src/main/java/com/victor/netflixclone/MoviesList.kt
+++ NetflixClone/app/src/main/java/com/victor/netflixclone/MoviesList.kt
@@ -1,11 +1,44 @@
 package com.victor.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.victor.netflixclone.databinding.ActivityMoviesListBinding
 
 class MoviesList : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMoviesListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movies_list)
+        binding = ActivityMoviesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                backScreenLogin()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun backScreenLogin() {
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }

1b110ecd02d24dab3ef5c0a9cbbc9c3aa7c1ca3e
MrVictor42 <guin-hu@hotmail.com>
2021-04-11 16:40:15 -0300

    Authenticate user

--- NetflixClone/app/src/main/java/com/victor/netflixclone/FormLogin.kt
+++ NetflixClone/app/src/main/java/com/victor/netflixclone/FormLogin.kt
@@ -3,6 +3,8 @@ package com.victor.netflixclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.victor.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
@@ -19,6 +21,31 @@ class FormLogin : AppCompatActivity() {
         binding.registerText.setOnClickListener {
             val intent = Intent(this, FormRegister::class.java)
             startActivity(intent)
+        }
+
+        binding.btnEnter.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+            val message = binding.messageError
+
+            if(email.isEmpty() || password.isEmpty()) {
+                message.text = "Fill All Fields"
+            } else {
+                authenticateUser(email, password)
+            }
+        }
+    }
+
+    private fun authenticateUser(email: String, password: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(this, "Successfully Logged In", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ListMovies::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener {
+            binding.messageError.text = "Error Login User"
         }
     }
 }

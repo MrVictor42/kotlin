26a5a69653f2ad6f0a9f5fae176f621ce56c3969
MrVictor42 <guin-hu@hotmail.com>
2021-04-11 12:15:32 -0300

    insert methods to register user in firebase

--- NetflixClone/app/src/main/java/com/victor/netflixclone/FormRegister.kt
+++ NetflixClone/app/src/main/java/com/victor/netflixclone/FormRegister.kt
@@ -2,6 +2,8 @@ package com.victor.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.victor.netflixclone.databinding.ActivityFormRegisterBinding
 
 class FormRegister : AppCompatActivity() {
@@ -14,5 +16,34 @@ class FormRegister : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+            val message = binding.message
+
+            if(email.isEmpty() || password.isEmpty()) {
+                message.text = "Fill All Fields!"
+            } else {
+                registerUser()
+            }
+        }
+    }
+
+    private fun registerUser() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
+        val message = binding.message
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(this, "Registered User Successfully!", Toast.LENGTH_SHORT).show()
+                binding.editEmail.setText("")
+                binding.editPassword.setText("")
+                message.text = ""
+            }
+        }.addOnFailureListener {
+            message.text = "Error Register User"
+        }
     }
 }

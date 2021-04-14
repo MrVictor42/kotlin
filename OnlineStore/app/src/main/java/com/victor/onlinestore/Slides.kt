ef5a412e6c4be2704e9b50bb093a2e42ae56a913
MrVictor42 <guin-hu@hotmail.com>
2021-04-14 15:19:53 -0300

    Create slides

--- OnlineStore/app/src/main/java/com/victor/onlinestore/Slides.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/Slides.kt
@@ -2,10 +2,28 @@ package com.victor.onlinestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
 
-class Slides : AppCompatActivity() {
+class Slides : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_slides)
+
+        addSlide(
+            SimpleSlide.Builder()
+                .background(R.color.purple)
+                .image(R.drawable.drawer)
+                .title("Online Store of Shoes")
+                .description("Enter And Check The Promotions We Have Prepared For You!")
+                .build()
+        )
+
+        addSlide(
+            SimpleSlide.Builder()
+                .background(R.color.blue_green)
+                .title("Create a Free Account")
+                .description("Register Right Now! And Come Meet Our Products!")
+                .build()
+        )
     }
 }

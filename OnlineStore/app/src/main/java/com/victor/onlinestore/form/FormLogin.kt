007a19a348872be7a8a5d3e636911450bd445e40
MrVictor42 <guin-hu@hotmail.com>
2021-04-14 22:26:31 -0300

    Config firebase in app. Register user and start methods to authenticate

--- OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormLogin.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormLogin.kt
@@ -1,8 +1,12 @@
 package com.victor.onlinestore.form
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.victor.onlinestore.R
 import com.victor.onlinestore.databinding.ActivityFormLoginBinding
 import com.victor.onlinestore.databinding.ActivityFormRegisterBinding
@@ -21,6 +25,28 @@ class FormLogin : AppCompatActivity() {
         binding.registerText.setOnClickListener {
             val intent = Intent(this, FormRegister::class.java)
             startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            authenticateUser()
+        }
+    }
+
+    private fun authenticateUser() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            val snarckbar = Snackbar.make(binding.layoutLogin, "Inform Your Email and Password!", Snackbar.LENGTH_INDEFINITE)
+            snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+            }).setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).show()
+        } else {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if(it.isSuccessful) {
+
+                }
+            }
         }
     }
 }
--- OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormRegister.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormRegister.kt
@@ -1,14 +1,64 @@
 package com.victor.onlinestore.form
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.victor.onlinestore.R
+import com.victor.onlinestore.databinding.ActivityFormRegisterBinding
 
 class FormRegister : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_register)
+        binding = ActivityFormRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        binding.btnRegister.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            val snarckbar = Snackbar.make(binding.layoutRegister, "Inform Your Email And Your Password!", Snackbar.LENGTH_INDEFINITE)
+            snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+            })
+            snarckbar.show()
+        } else {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if(it.isSuccessful) {
+                    val snarckbar = Snackbar.make(binding.layoutRegister, "User Successfully Registered!", Snackbar.LENGTH_INDEFINITE)
+                    snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+                        backToLogin()
+                    })
+                    snarckbar.show()
+                }
+            }.addOnFailureListener {
+                val snarckbar = Snackbar.make(binding.layoutRegister, "Error When Registering User!", Snackbar.LENGTH_INDEFINITE)
+                snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                })
+                snarckbar.show()
+            }
+        }
+    }
+
+    private fun backToLogin() {
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }

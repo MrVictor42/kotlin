00f82c4442e0a5a35dc4cc91eb07a1c39a5836cc
MrVictor42 <guin-hu@hotmail.com>
2021-04-15 07:51:53 -0300

    Method logout from firebase and a loading screen when user is successful login

--- OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormLogin.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/form/FormLogin.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.victor.onlinestore.MainScreen
 import com.victor.onlinestore.R
 import com.victor.onlinestore.databinding.ActivityFormLoginBinding
 import com.victor.onlinestore.databinding.ActivityFormRegisterBinding
@@ -22,6 +24,8 @@ class FormLogin : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        userIsLogged()
+
         binding.registerText.setOnClickListener {
             val intent = Intent(this, FormRegister::class.java)
             startActivity(intent)
@@ -36,17 +40,37 @@ class FormLogin : AppCompatActivity() {
         val email = binding.editEmail.text.toString()
         val password = binding.editPassword.text.toString()
 
-        if(email.isEmpty() || password.isEmpty()) {
+        if (email.isEmpty() || password.isEmpty()) {
             val snarckbar = Snackbar.make(binding.layoutLogin, "Inform Your Email and Password!", Snackbar.LENGTH_INDEFINITE)
             snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
 
             }).setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).show()
         } else {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                if(it.isSuccessful) {
-
+                if (it.isSuccessful) {
+                    binding.frameLogin.visibility = View.VISIBLE
+                    Handler().postDelayed({
+                        val intent = Intent(this, MainScreen::class.java)
+                        startActivity(intent)
+                        finish()
+                    }, 3000)
                 }
+            }.addOnFailureListener {
+                val snarckbar = Snackbar.make(binding.layoutLogin, "Error Login User!", Snackbar.LENGTH_INDEFINITE)
+                snarckbar.setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).setAction("OK", View.OnClickListener {
+
+                }).setBackgroundTint(Color.WHITE).setTextColor(Color.BLACK).show()
             }
+        }
+    }
+
+    private fun userIsLogged() {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        if(currentUser != null) { //login was successful
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }

f6ae8961f6582a9c415d21ab63d0dde70d0bb774
MrVictor42 <guin-hu@hotmail.com>
2021-04-15 16:49:41 -0300

    Upload images from cellphone galery

--- OnlineStore/app/src/main/java/com/victor/onlinestore/fragments/RegisterProduct.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/fragments/RegisterProduct.kt
@@ -1,12 +1,43 @@
 package com.victor.onlinestore.fragments
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.victor.onlinestore.R
+import com.victor.onlinestore.databinding.ActivityRegisterProductBinding
 
 class RegisterProduct : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterProductBinding
+    private var selectedURI: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_product)
+        binding = ActivityRegisterProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSelectPhoto.setOnClickListener {
+            selectPhotoFromGallery()
+        }
+    }
+
+    private fun selectPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0) {
+            selectedURI = data?.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedURI)
+            binding.productPhoto.setImageBitmap(bitmap)
+            binding.btnSelectPhoto.alpha = 0f
+        }
     }
 }

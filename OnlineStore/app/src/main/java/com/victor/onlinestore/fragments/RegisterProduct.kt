777fc54afd8eecddc16560bb2be2704da47dfe60
MrVictor42 <guin-hu@hotmail.com>
2021-04-15 23:02:11 -0300

    Insert firebasefirestone to sabe products in cloud

--- OnlineStore/app/src/main/java/com/victor/onlinestore/fragments/RegisterProduct.kt
+++ OnlineStore/app/src/main/java/com/victor/onlinestore/fragments/RegisterProduct.kt
@@ -5,8 +5,14 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.victor.onlinestore.R
 import com.victor.onlinestore.databinding.ActivityRegisterProductBinding
+import com.victor.onlinestore.model.Data
+import java.util.*
 
 class RegisterProduct : AppCompatActivity() {
 
@@ -20,6 +26,33 @@ class RegisterProduct : AppCompatActivity() {
 
         binding.btnSelectPhoto.setOnClickListener {
             selectPhotoFromGallery()
+        }
+
+        binding.btnRegisterProduct.setOnClickListener {
+            saveInFirebase()
+        }
+    }
+
+    private fun saveInFirebase() {
+        val file = UUID.randomUUID().toString()
+        val reference = FirebaseStorage.getInstance().getReference("/images/${file}")
+        selectedURI?.let {
+            reference.putFile(it).addOnSuccessListener {
+                reference.downloadUrl.addOnSuccessListener {
+                    val url = it.toString()
+                    val name = binding.editNameProduct.text.toString()
+                    val price = "R$ " + binding.editPrice.text.toString()
+                    val uid = FirebaseAuth.getInstance().uid
+                    val Products = Data(url, name, price)
+                    //cria uma coleção de produtos no caso
+                    FirebaseFirestore.getInstance().collection("Products")
+                    .add(Products).addOnSuccessListener {
+                        Toast.makeText(this, "Product Registration Successful", Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener {
+
+                    }
+                }
+            }
         }
     }
 
